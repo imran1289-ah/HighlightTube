9906d83bceebaeb95bb8841c3b0fbee3ad196f8f
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-05-14 19:27:34 -0400

    Adding Mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017' , {useNewUrlParser:true})
+    .then(() => console.log("Database connected"))
+    .catch(err => console.error(err));
 
 app.get('/', (req,res) =>{
     res.send('hello world')
