08b3ac1f23fd6e9a85e00cd0d132831c3d70e5db
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-05-15 17:52:00 -0400

    Adding the register endpoint

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const {User} = require('./model/user');
 
-
+//connection to mongoDB database
 mongoose.connect('mongodb://localhost:27017' , {useNewUrlParser:true})
     .then(() => console.log("Database connected"))
     .catch(err => console.error(err));
 
-app.get('/', (req,res) =>{
-    res.send('hello world')
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+//endpoint to register
+app.post('/user/register', (req,res) => {
+    const user = new User(req.body)
+    user.save((err, userData) =>{
+        if(err) return res.json ({success: false,err})
+    })
+    return res.status(200).json({
+        success:true
+    })
 })
 
 app.listen(5000);
