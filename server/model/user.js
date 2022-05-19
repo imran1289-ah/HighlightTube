646255aa9cc5ed44cabeecc4a52e5d06d0bf4dca
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-05-19 19:49:00 -0400

    Adding the functionality of hashing password

--- server/model/user.js
+++ server/model/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name:{
@@ -22,6 +24,31 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 })
+
+// this function will hash the password before the user is saved in the database
+userSchema.pre('save', function (next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(error, salt){
+            if(error){
+                return next(error);
+            }
+
+            bcrypt.hash(user.password, salt, function(error, hash){
+                if(error){
+                    return next(error);
+                    user.password = hash
+                }
+            })    
+        })
+
+    }
+    else{
+        next ()
+    }
+});
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = {User}
