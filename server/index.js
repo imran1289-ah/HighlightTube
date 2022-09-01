de8bb8e63ea1952b58a55bfdd11481fc58ceaa24
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-09-01 12:42:37 -0400

    Add models and starting to build the REST API

--- server/index.js
+++ server/index.js
@@ -1,86 +1,29 @@
-const express = require('express');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import commentRoutes from "./routes/comments.js";
+import videoRoutes from "./routes/videos.js";
+
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const {User} = require('./model/user');
-const config = require('./config/key');
-const {auth} = require('./midleware/auth');
+dotenv.config();
 
-//connection to mongoDB database
-mongoose.connect(config.mongoURI , {useNewUrlParser:true})
-    .then(() => console.log("Database connected"))
-    .catch(err => console.error(err));
-
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-
-//endpoin for autenticatiom
-app.get('/user/auth', auth, (req,res) => {
-    res.status(200).json({
-        _id: req._id,
-        isAuth: true,
-        email:req.user.email,
-        name:req.user.name,
-        lastname: req.user.lastname,
-        role: req.user.role
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to DataBase");
     })
-})
+    .catch((err) => {
+      throw err;
+    });
+};
 
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
-//endpoint to register
-app.post('/user/register',  (req,res) => {
-    const user = new User(req.body)
-    user.save((err, userData) =>{
-        if(err) return res.json ({success: false,err})
-    })
-    return res.status(200).json({
-        success:true
-    })
-})
-
-//endpoint to login
-app.post('/user/login', (req,res) => {
-    User.findOne({email: req.body.email}, (err, user) => {
-        if(!user){
-            return res.json({
-                loginSuccess: false,
-                message: "Email not found"
-            })
-        }
-
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if(!isMatch){
-                return res.json({
-                    loginSuccess: false,
-                    message: "Wrong password ! Please try again"
-                })
-            }
-        })
-
-        user.generateToken((error, user) => {
-            if(error) return res.status(400).send(error);
-            res.cookie("x_auth", user.token)
-                .status(200)
-                .json({
-                    loginSuccess: true
-                })
-        })
-
-    })
-})
-
-
-//endpoint to logout
-app.get('/user/logout', auth ,(req,res) => {
-    User.findOneAndUpdate({_id: req.user._id}, {token : "", tokenExp: ""}, (err,doc)=>{
-        if(err) return res.json({success:false, err});
-        return res.status(200).send({
-            success:true
-        })
-    })
-})
-
-app.listen(5000);
+app.listen(9000, () => {
+  connect();
+  console.log("Connected to HighlighTube !");
+});
--- server/model/user.js
+++ server/model/user.js
@@ -1,90 +1,30 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const jwt = require('jsonwebtoken');
+import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-    name:{
-        type:String
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type:String
+      type: String,
+      required: true,
+      unique: true,
     },
-    password : {
-        type:String,
-        minglength : 6
+    password: {
+      type: String,
+      required: true,
     },
-    role :{
-        type: Number,
-        default:0
+    subscribers: {
+      type: Number,
+      default: 0,
     },
-    token:{
-        type:String,
+    subscribedUsers: {
+      type: [String],
     },
-    tokenExp:{
-        type:Number
-    }
-})
+  },
+  { timestamps: true }
+);
 
-// this function will hash the password before the user is saved in the database
-userSchema.pre('save', function (next){
-    var user = this;
-
-    if(user.isModified('password')){
-        bcrypt.genSalt(saltRounds, function(error, salt){
-            if(error){
-                return next(error);
-            }
-
-            bcrypt.hash(user.password, salt, function(error, hash){
-                if(error) return next(error);
-                user.password = hash
-                next();
-                
-            })    
-        })
-
-    }
-    else{
-        next ()
-    }
-});
-
-//this function will compare passwords
-userSchema.methods.comparePassword = function(plainPassword, cb){
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if(err) return cb(err);
-        cb(null, isMatch)
-    } )
-}
-
-//this function will generate a token
-userSchema.methods.generateToken = function(cb) {
-    var user = this;
-    var token = jwt.sign(user._id.toHexString(), 'secret')
-
-    user.token = token;
-    user.save(function (error, user){
-        if(error) return cb(error)
-        cb(null, user);
-    })
-}
-
-userSchema.statics.findByToken = function (token, cb) {
-    var user = this;
-
-    jwt.verify(token, 'secret', function(err, decode){
-        user.findOne({
-            "_id":decode,
-            "token":token,
-            function(err,user){
-                if(err) return cb(err);
-                cb(null, user);
-            }
-        })
-    })
-}
-
-
-const User = mongoose.model('User', userSchema);
-module.exports = {User}
+export default mongoose.model("User", UserSchema);
