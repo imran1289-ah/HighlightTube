6a36782de25b9d4792d0d85d2228078d56e58ae5
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-09-03 13:57:46 -0400

    Adding routes for signing in and signing up

---
+++ server/controllers/auth.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import User from "../model/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+    await newUser.save();
+    res.status(200).send("User created");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ name: req.body.name });
+
+    if (!user) {
+      return next(createError(404, "User does not exist"));
+    }
+
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isCorrect) {
+      return next(createError(400, "Wrong password"));
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password, ...others } = user._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  } catch (err) {
+    next(err);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/users.js";
 import commentRoutes from "./routes/comments.js";
 import videoRoutes from "./routes/videos.js";
+import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -19,9 +21,23 @@ const connect = () => {
     });
 };
 
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Error";
+  return res.status(status).json({
+    sucess: false,
+    status,
+    message,
+  });
+});
 
 app.listen(9000, () => {
   connect();
