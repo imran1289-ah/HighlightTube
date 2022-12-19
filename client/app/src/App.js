c770bea7cc666d0879af2a33228e8e6d1330f6d9
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-12-19 09:49:06 -0500

    fetching videos from server

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -29,6 +29,17 @@ function App() {
               <Routes>
                 <Route path="/">
                   <Route index element={<Home />}></Route>
+                  <Route index element={<Home type="random" />}></Route>
+                  <Route
+                    index
+                    path="trends"
+                    element={<Home type="trend" />}
+                  ></Route>
+                  <Route
+                    index
+                    path="subscriptions"
+                    element={<Home type="sub" />}
+                  ></Route>
                   <Route path="signin" element={<SignIn />}></Route>
                   <Route path="/video/test" element={<Video />}></Route>
                 </Route>
--- client/app/src/pages/Home.jsx
+++ client/app/src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Card from "../components/Card";
+import axios from "axios";
 
 const Container = styled.div`
   display: flex;
@@ -9,18 +10,22 @@ const Container = styled.div`
   background-color: #202020;
 `;
 
-const Home = () => {
+const Home = ({ type }) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get(`/videos/${type}`);
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, [type]);
+
   return (
     <Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {videos.map((video) => (
+        <Card />
+      ))}
     </Container>
   );
 };
